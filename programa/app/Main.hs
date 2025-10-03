7868debebec83d29a57eda9eee2b4f5a43d2e448
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-10-03 08:47:52 -0600

    importar ventas terminado

--- programa/app/Main.hs
+++ programa/app/Main.hs
@@ -5,11 +5,24 @@ module Main where
 import Utilidades
 
 main :: IO ()
-main = do
-    ventasActuales <- cargarVentas "src/data/Ventas.json"
-    case ventasActuales of
-        Left err ->
-            putStrLn $ "Error al cargar ventas: " ++ err
-        Right ventas -> do
-            putStrLn "Ventas cargadas:"
-            mapM_ print ventas
+
+
+
+
+
+
+-- Main para probar las funciones de cargar e importar ventas
+--main = do
+--    ventasActuales <- cargarVentasDeArchivo "src/data/Ventas.json"
+--   case ventasActuales of
+--        Left err ->
+--            putStrLn $ "Error al cargar ventas: " ++ err
+--        Right ventas -> do
+--            putStrLn "Ventas cargadas:"
+--            mapM_ print ventas
+--            resultadoImportacion <- importarVenta "src/lote.json" ventas
+--            case resultadoImportacion of
+--                Left err -> putStrLn $ "Error al importar ventas: " ++ err
+--                Right ventasActualizadas -> do
+--                    putStrLn "Ventas actualizadas:"
+--                    mapM_ print ventasActualizadas
--- programa/src/Utilidades.hs
+++ programa/src/Utilidades.hs
@@ -2,15 +2,24 @@
 module Utilidades where
 import System.Directory (doesFileExist)
 import System.IO
-import Data.Aeson (eitherDecode)
+import Data.Aeson (eitherDecode, encode)
 import qualified Data.ByteString.Lazy as B
 import Venta
 
-cargarVentas :: FilePath -> IO (Either String [Venta])
-cargarVentas ruta = do
+cargarVentasDeArchivo :: FilePath -> IO (Either String [Venta])
+cargarVentasDeArchivo ruta = do
     contenido <- B.readFile ruta
     let resultado = eitherDecode contenido :: Either String [Venta]
     return resultado
 
 
-
+importarVenta :: FilePath -> [Venta] -> IO (Either String [Venta])
+importarVenta ruta listaVentasActual = do 
+    contenidoByteString <- B.readFile ruta 
+    let contenidoJSON = eitherDecode contenidoByteString :: Either String [Venta]
+    case contenidoJSON of
+        Left err -> return (Left err)
+        Right ventasNuevas -> do
+            let listaVentasActualizadas = listaVentasActual ++ ventasNuevas
+            B.writeFile "src/data/Ventas.json" (encode listaVentasActualizadas)
+            return (Right listaVentasActualizadas)
