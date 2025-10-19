a4944783f395aa38da2ba1b9c436dbc4df13936d
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-10-19 00:46:29 -0600

    Acualizacion Utilidades

--- programa/src/Utilidades.hs
+++ programa/src/Utilidades.hs
@@ -14,13 +14,16 @@ module Utilidades where
 
 import System.Directory (doesFileExist)
 import System.IO
-import Data.Aeson (eitherDecode, encode)
+import Data.Aeson (eitherDecode, encode, ToJSON, toJSON, object, (.=))
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy as B
 import Venta
 import Data.List (nubBy)
 import Data.Function (on)
 import Venta
+import Estadisticas
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Format (formatTime, defaultTimeLocale)
 
 -- ----------------------------------------------------------------------------- 
 -- | Carga una lista de ventas desde un archivo JSON.
@@ -169,3 +172,117 @@ diasSemanaES dia = case dia of
     "Saturday"  -> "Sábado"
     "Sunday"    -> "Domingo"
     _           -> dia
+
+-- ----------------------------------------------------------------------------- 
+-- | Guarda la lista de ventas en un archivo JSON legible.
+--  Autor: Geovanni Gonzalez
+--
+-- ==== Parámetros
+-- * 'ventas' : Lista de ventas a guardar.
+-- ==== Retorno
+-- Acción IO que escribe el archivo JSON.
+-- ==== Ejemplo
+-- >>> guardarVentasEnArchivo ventas
+guardarVentasEnArchivo :: [Venta] -> IO ()
+guardarVentasEnArchivo ventas = do
+    B.writeFile "src/data/Ventas.json" (encodePretty ventas)
+    putStrLn "Datos guardados en src/data/Ventas.json"
+
+-- -----------------------------------------------------------------------------
+-- | Muestra los detalles de una venta en la consola.
+--  Autor: Geovanni Gonzalez
+-- ==== Parámetros
+-- * 'venta' : Venta a mostrar.
+-- ==== Retorno
+-- Acción IO que imprime los detalles de la venta.
+-- ==== Ejemplo
+-- >>> mostrarVenta venta
+mostrarVenta :: Venta -> IO ()
+mostrarVenta venta = do
+    putStrLn $ "  ID: " ++ show (idVenta venta) ++ 
+               " | Fecha: " ++ fecha venta ++ 
+               " | Producto: " ++ nombreProducto venta ++
+               " | Categoría: " ++ categoria venta ++
+               " | Cantidad: " ++ show (cantidad venta) ++
+               " | Precio: $" ++ show (precioUnitario venta) ++
+               " | Total: $" ++ show (totalVenta venta)
+
+--------------------------------------------------------------------------------
+-- Funciones para exportar reportes fijos en JSON
+--------------------------------------------------------------------------------
+
+-- -----------------------------------------------------------------------------
+-- | Exporta el top 5 de categorías con mayores ventas a un archivo JSON fijo.
+--  Autor: Gerny Diaz
+-- ==== Parámetros
+-- * 'top5' : Lista de tuplas (categoría, monto total).
+-- ==== Retorno
+-- Acción IO que escribe el archivo JSON.
+-- ==== Ejemplo
+-- >>> exportarTop5CategoriasJSONFijo top5
+exportarTop5CategoriasJSONFijo :: [(String, Int)] -> IO ()
+exportarTop5CategoriasJSONFijo top5 = do
+    tiempo <- getCurrentTime
+    let fecha = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" tiempo
+    B.writeFile "src/data/top5categorias.json" $ encodePretty $
+        object [ "titulo" .= ("Top 5 Categorías con Mayores Ventas" :: String)
+               , "fecha_generacion" .= fecha
+               , "categorias" .= map (\(c, m) -> object ["categoria" .= c, "monto" .= m]) top5
+               ]
+    putStrLn "Top 5 categorías exportado a /programa/data/top5categorias.json"
+
+-----------------------------------------------------------------------------
+-- | Exporta el producto más vendido a un archivo JSON fijo.
+--  Autor: Gerny Diaz
+-- ==== Parámetros
+-- * 'producto' : Nombre del producto más vendido.
+-- * 'cantidad' : Cantidad vendida del producto más vendido.
+exportarProductoMasVendidoJSONFijo :: String -> Int -> IO ()
+exportarProductoMasVendidoJSONFijo producto cantidad = do
+    tiempo <- getCurrentTime
+    let fecha = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" tiempo
+    B.writeFile "src/data/productomasvendido.json" $ encodePretty $
+        object [ "titulo" .= ("Producto Más Vendido" :: String)
+               , "fecha_generacion" .= fecha
+               , "producto" .= producto
+               , "cantidad" .= cantidad
+               ]
+    putStrLn "Producto más vendido exportado a /programa/data/productomasvendido.json"
+
+-- -----------------------------------------------------------------------------
+-- | Exporta la categoría con menor participación a un archivo JSON fijo.
+--  Autor: Geovanni Gonzalez
+-- ==== Parámetros
+-- * 'cat' : Nombre de la categoría con menor participación.
+-- * 'cant' : Cantidad total vendida en esa categoría.
+exportarCategoriaMenorParticipacionJSONFijo :: String -> Int -> IO ()
+exportarCategoriaMenorParticipacionJSONFijo cat cant = do
+    tiempo <- getCurrentTime
+    let fecha = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" tiempo
+    B.writeFile "src/data/categoriamenor.json" $ encodePretty $
+        object [ "titulo" .= ("Categoría con Menor Participación" :: String)
+               , "fecha_generacion" .= fecha
+               , "categoria" .= cat
+               , "cantidad_total" .= cant
+               ]
+    putStrLn "Categoría con menor participación exportada a /programa/data/categoriamenor.json"
+
+------------------------------------------------------------------------------
+-- | Exporta el resumen general de ventas a un archivo JSON fijo.
+--  Autor: Geovanni Gonzalez
+-- ==== Parámetros
+-- * 'resumen' : ResumenGeneral con los datos a exportar.
+
+exportarResumenGeneralJSONFijo :: ResumenGeneral -> IO ()
+exportarResumenGeneralJSONFijo resumen = do
+    tiempo <- getCurrentTime
+    let fecha = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" tiempo
+    B.writeFile "src/data/resumengeneral.json" $ encodePretty $
+        object [ "titulo" .= ("Resumen General de Ventas" :: String)
+               , "fecha_generacion" .= fecha
+               , "cantidadPorCategoria" .= cantidadPorCategoria resumen
+               , "ventaMasAlta" .= ventaMasAlta resumen
+               , "ventaMasBaja" .= ventaMasBaja resumen
+               , "categoriaConMayorVariedad" .= categoriaConMayorVar resumen
+               ]
+    putStrLn "Resumen general exportado a /programa/data/resumengeneral.json"
